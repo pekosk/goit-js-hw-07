4b1d5598e70461d603a4f6be4981d41f9b40d1eb
pekosk <rkostyk64@gmail.com>
2021-11-15 15:50:25 +0200

    2 таск

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryItem = createCard(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryItem);
+
+function createCard(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+}).join('');
+}
+
+new SimpleLightbox('.gallery a', {
+captionsData: 'alt',
+captionsDelay: 250,
+});
+
+lightbox = new SimpleLightbox('.gallery a', ({ SourceAttr }));
 
 console.log(galleryItems);
